8c96f03ce24050a7a8c39c5d606838aea8da58b3
Kiryl Khadanovich <khodkp@gmail.com>
2016-12-16 23:57:43 +0300

    I don't know. Something with crypto in new branch

---
+++ MyCryptography/Crypto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCryptography
+{
+    class Crypto
+    {
+        private string _key ;
+
+        private string Key
+        {
+            get { return _key; }
+            set { _key = value; }
+        }
+
+        public Crypto(string key)
+        {
+            Key = key;
+        }
+
+        public string Encrypt(string toEncrypt)
+        {
+            int tmp = int.Parse(toEncrypt) ^ byte.Parse(Key);
+
+        }
+    }
+}
